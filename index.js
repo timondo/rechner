28d3f0df1a2aa6ea03bf1367c8230103ef37fc2b
timondo <timon@klingel-stade.de>
2025-04-30 17:59:11 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
-// sin Rechner
-function berechnenSin() {
-    let singegenkathete = parseFloat(document.querySelector("#sinGegenkathete").value);
-    let sinhypotenuse = parseFloat(document.querySelector("#sinHypotenuse").value);
-
-    if (!isNaN(singegenkathete) && !isNaN(sinhypotenuse)) {
-        document.getElementById("sinoutput").innerHTML = (singegenkathete / sinhypotenuse).toFixed(2);
-    } else {
-        alert("Bitte geben Sie korrekte Werte ein");
-    }
+function sin() {
+    const formel = "Gegenkathete/Hypotenose";
+    document.getElementById('formel').innerHTML = formel;
+    document.getElementById('x').placeholder = "Gegenkathete";
+    document.getElementById('y').placeholder = "Hypotenuse";
 }
 
-// cos Rechner
-function berechnenCos() {
-    let cosankathete = parseFloat(document.querySelector("#Ankathete").value);
-    let coshypotenuse = parseFloat(document.querySelector("#Hypothenuse2").value);
-
-    if (!isNaN(cosankathete<coshypotenuse) && !isNaN(coshypotenuse)) {
-        document.getElementById("cosoutput").innerHTML = (cosankathete / coshypotenuse).toFixed(2);
-    } else {
-        alert("Bitte geben Sie korrekte Werte ein");
-    }
+function cos() {
+    const formel = "Ankathete/Hypotenuse";
+    document.getElementById('formel').innerHTML = formel;
+    document.getElementById('x').placeholder = "Ankathete";
+    document.getElementById('y').placeholder = "Hypotenuse";
 }
 
-// tan Rechner
-function berechnenTan() {
-    let tangegenkathete = parseFloat(document.querySelector("#Gegenkathete2").value);
-    let tanankathete = parseFloat(document.querySelector("#Ankathete2").value);
-
-    if (!isNaN(tangegenkathete) && !isNaN(tanankathete)) {
-        document.getElementById("tanoutput").innerHTML = (tangegenkathete / tananakathete).toFixed(2);
-    } else {
-        alert("Bitte geben Sie korrekte Werte ein");
-    }
+function tans() {
+    const formel = "Gegenkathete/Ankathete";
+    document.getElementById('formel').innerHTML = formel;
+    document.getElementById('x').placeholder = "Gegenkathete";
+    document.getElementById('y').placeholder = "Ankathete";
 }
+
+function berechnen() {
+    let x = parseFloat(document.querySelector("#x").value);
+    let y = parseFloat(document.querySelector("#y").value);
+    if (!isNaN(x) && !isNaN(y)){
+    document.getElementById('Ergebnis').innerHTML = x/y;
+  }
+    else {
+    alert("Bitte geben sie gültige Werte ein")}
+     }
+   
+debugger
